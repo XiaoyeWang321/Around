896514c744a6370375ddf4b717fe073a14af62c1
Xiaoye_Wang <wangxiaoye1012@hotmail.com>
2018-09-05 20:04:40 -0400

    8th

--- service/main.go
+++ service/main.go
@@ -137,7 +137,7 @@ const (
 	PROJECT_ID = "around-215120"
 	BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://104.196.130.45 :9200"
+	ES_URL = "http://104.196.130.45:9200"
 )
 
 func containsFilteredWords(s *string) bool {
